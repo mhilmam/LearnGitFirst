2f0a90b316fe4704a6da7bcfe445c1ac12817836
mhilmam <mandaneo007@gmail.com>
2022-03-08 22:25:45 +0700

    project pertama menggunakan git dan gitlab

---
+++ app/src/main/java/com/binar/constraintlayout/ActivitySignIn.kt
@@ -0,0 +1,11 @@
+package com.binar.constraintlayout
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivitySignIn : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
